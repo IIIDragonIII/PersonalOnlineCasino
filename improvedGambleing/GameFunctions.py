0a9c90a9e4b112e1624dad6ef2e2ddda36178bc0
IIIDragonIII <joriswehausen@gmail.com>
2026-06-30 20:41:31 +0200

    Update

--- improvedGambleing/GameFunctions.py
+++ improvedGambleing/GameFunctions.py
@@ -24,24 +24,70 @@ class Jetons:
                     return einsatz
             except ValueError:
                 print("Ungültige Eingabe. Bitte geben Sie eine Zahl ein.")
+    def Auszahlung(self, einsatz, BJ):
+        with open("jetons.txt", "r") as f:
+            jetons = int(f.read())
+        if BJ:
+            gewinn = round((einsatz * 2.25), 0)
+            with open("jetons.txt", "w") as f:
+                f.write(str(jetons + gewinn))
+        else:
+            gewinn = einsatz * 2
+            with open("jetons.txt", "w") as f:
+                f.write(str(jetons + gewinn))
 
-class Deck:
+class Blackjack:
     #Kartendeck und Kartenfunktionen erstellen
     karten = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
     symbole = ["♠", "♥", "♦", "♣"]
     kartenstapel = []
+    kartenWert = {"2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9, "10": 10, "J": 10, "Q": 10, "K": 10, "A": 11}
+    spieler_blackjack = False
+    dealer_blackjack = False
+    no_comparison = False
+    dealer_win = False
+    spieler_win = False
+    cause = ""
 
-    def createDeck(self):
+    def DeckErstellen(self):
         for symbol in self.symbole:
             for karte in self.karten:
                 self.kartenstapel.append((karte, symbol))
-    def mischen(self):
+    def Mischen(self):
         random.shuffle(self.kartenstapel)
-    def ziehen(self):
+    def Ziehen(self):
         if len(self.kartenstapel) > 0:
             gezogen = self.kartenstapel.pop()
-            print(f"Sie haben {gezogen[0]}{gezogen[1]} gezogen.")
-            return gezogen
+            wert = self.kartenWert[gezogen[0]]
+            anzeige = f"{gezogen[1]}{gezogen[0]}"
+            return anzeige, wert
         else:
             print("Der Kartenstapel ist leer.")
             return None
+    def KartenBlackjack(self, spieler_gesamt_wert, dealer_gesamt_wert):
+        if spieler_gesamt_wert == 21:
+            self.spieler_blackjack = True
+            self.no_comparison = True
+            return self.spieler_blackjack
+        elif dealer_gesamt_wert == 21:
+            self.dealer_blackjack = True
+            self.no_comparison = True
+            return self.dealer_blackjack
+    def ÜberEinundzwanzig(self, spieler_gesamt_wert, dealer_gesamt_wert):
+        
+        if spieler_gesamt_wert > 21:
+            self.dealer_win, self.cause = True, "Spieler hat über 21"
+        elif dealer_gesamt_wert > 21:
+            self.spieler_win, self.cause = True, "Dealer hat über 21"
+    def EndKartenWert(self, spieler_gesamt_wert, dealer_gesamt_wert):
+        if spieler_gesamt_wert > dealer_gesamt_wert:
+            self.spieler_win, self.cause = True, "Spieler hat mehr"
+        elif spieler_gesamt_wert < dealer_gesamt_wert:
+            self.dealer_win, self.cause = True, "Dealer hat mehr"
+        elif spieler_gesamt_wert == dealer_gesamt_wert:
+            self.cause = "Unentschieden"
+
+    def GameVariableReset(self):
+        self.spieler_blackjack = False
+        self.dealer_blackjack = False
+        self.no_comparison = False
--- improvedGambleing/gambleingImproved.py
+++ improvedGambleing/gambleingImproved.py
@@ -1,10 +1,10 @@
 import pygame
 import random
 import os
-import improvedGambleing.GameFunctions as GameFunctions 
-from improvedGambleing.GameFunctions import Deck, Jetons
+import GameFunctions
+from GameFunctions import Blackjack, Jetons
 
-deck = Deck()
+bj = Blackjack()
 jetons = Jetons()
 start = False
 
@@ -18,10 +18,31 @@ while not start:
     else:
         print("Fehler beim Setzen der Jetons.")
         start = False
+
 #Deck erstellen und mischen
-deck.createDeck()
-deck.mischen()
+bj.DeckErstellen()
+bj.Mischen()
 
 #Karten für Spieler ziehen
-print("1. Karte:", deck.ziehen())
-print("2. Karte:", deck.ziehen())
+erste_karte, erster_wert = bj.Ziehen()
+zweite_karte, zweiter_wert = bj.Ziehen()
+gesamt_wert = erster_wert + zweiter_wert
+print("1. Karte:", erste_karte, "2. Karte:", zweite_karte, "Gesamt:", gesamt_wert)
+
+#Karten für Dealer ziehen
+dealer_erste_karte, dealer_erster_wert = bj.Ziehen()
+dealer_zweite_karte, dealer_zweiter_wert = bj.Ziehen()
+dealer_gesamt_wert = dealer_erster_wert + dealer_zweiter_wert
+print(f"Dealer: {dealer_erste_karte} [_] Gesamt: {dealer_erster_wert}",end="/r", flush=True)
+
+bj.KartenBlackjack(gesamt_wert, dealer_gesamt_wert)
+if bj.spieler_blackjack == True:
+    print(f"Dealer: {dealer_erste_karte} [_] Gesamt: {dealer_erster_wert}")
+    print("Spieler hat Blackjack!")
+    
+elif bj.dealer_blackjack == True:
+    print(f"Dealer: {dealer_erste_karte} {dealer_zweite_karte} Gesamt: {dealer_gesamt_wert}")
+    print("Dealer hat Blackjack!")
+    
+if not bj.no_comparison:
+    bj.ÜberEinundzwanzig(gesamt_wert, dealer_gesamt_wert)
